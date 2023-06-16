528708d1cd2394e40a4a4c6f6ed361538d9e542f
groverush <groverush@gmail.com>
2023-06-16 15:02:18 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import qrImage from "./assets/image-qr-code.png"
+const App = () => {
+  return (
+    <div className="flex flex-col justify-between h-screen w-screen">
+      <section className="max-w-[380px] mx-auto mt-20 rounded-xl bg-white p-5 pb-7">
+        <picture>
+          <img src={qrImage} alt="qr-image" className="rounded-xl" />
+        </picture>
+        <div className="mt-4 text-center">
+          <h3 className="font-bold text-3xl">
+            Improve your front-end <br />
+            skills by building projects
+          </h3>
+
+          <p className="text-gray-400 text-enter px-2 mt-5 text-lg tracking-wider leading-tight">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </section>
+
+      <footer className="text-center text-base">
+        Challenge by{" "}
+        <a
+          className="text-sky-400 font-semibold"
+          href="https://www.frontendmentor.io/home"
+          target="_blank"
+        >
+          Frontend Mentor.
+        </a>{" "}
+        Coded by
+        <a
+          className="text-sky-400 font-semibold"
+          href="https://app.netlify.com/teams/groverush/overview"
+          target="_blank"
+        >
+          Julian Monroy.
+        </a>
+      </footer>
+    </div>
+  )
+}
+export default App
